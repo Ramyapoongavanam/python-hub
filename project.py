ac22c851c39a719cd2861d1488cdbfe7e889cb74
Ramyapoongavanam <146525699+Ramyapoongavanam@users.noreply.github.com>
2024-05-11 20:33:35 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -1,23 +1,55 @@
-board=['_' for i in range(9)]
-def displayboard():
-    print("|", board[0], "|", board[1], "|", board[2], "|")
-    print("|", board[3], "|", board[4], "|", board[5], "|")
-    print("|", board[6], "|", board[7], "|", board[8], "|")
+# This function adds two numbers
+def add(x, y):
+    return x + y
 
-player1="x"
-player2="o"
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
 
-def startgame():
-    while True:
-        while True:
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
 
-        player1_option=input(f"{player1},enter your position: ")
-        if board[int(player1_option)-1]=="_":
-            board[int(player1_option)-1]==player1
-            displayBoard()
-            break
+# This function divides two numbers
+def divide(x, y):
+    return x / y
 
-        else:
-            print("This place is not empty !")
 
-startgame()
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
